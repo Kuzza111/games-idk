34e85b5d577c10ef2935b91c28f4626c3398169a
Kuzza <168814633+Kuzza111@users.noreply.github.com>
2026-02-26 04:49:19 +0300

    added saving results

--- cellSim.py
+++ cellSim.py
@@ -1,16 +1,18 @@
 import pygame
 import random
+import json
+from pathlib import Path
 
-SIM_WIDTH = 75
-SIM_HEIGHT = 75
-multiplier = 5
+SIM_WIDTH = 100
+SIM_HEIGHT = 100
+multiplier = 10
 
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.objects = []
-        self.objectsCoordinates = [[None for _ in range(self.width)] for _ in range(self.height)]
+        self.objectsCoordinates = [[None for _ in range(self.height)] for _ in range(self.width)]
         self.player = None
 
         self.lightMap = []
@@ -29,6 +31,19 @@ class World:
                 alifeObjects.append(self.objects[i])
         self.objects = alifeObjects
 
+        self.playerMovement()
+
+    def generateLightMap(self):
+        lMap = [[None for _ in range(self.height)] for _ in range(self.width)]
+        for i in range(len(lMap)):
+            for j in range(len(lMap[0])):
+                #lMap[i][j] = i-j
+                if i<len(lMap)//2: lMap[i][j] = (len(lMap)//2 - (len(lMap)//2-i))//4
+                else: lMap[i][j] = (len(lMap)-i)//4
+        self.lightMap = lMap
+        print(lMap)
+
+    def playerMovement(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
             self.player.moveForward()
@@ -38,27 +53,51 @@ class World:
             self.player.turnRight()
         if keys[pygame.K_SPACE]:
             self.player.replicate()
+        if keys[pygame.K_e]:
+            self.player.biteCell()
         self.player.energy = 100
 
-    def generateLightMap(self):
-        lMap = [[None for _ in range(self.height)] for _ in range(self.width)]
-        for i in range(len(lMap)):
-            for j in range(len(lMap[0])):
-                lMap[i][j] = (i + j)//6#int(self.height/i)
-        self.lightMap = lMap
-        print(self.lightMap)
-        print(len(self.lightMap), " ", len(self.lightMap[0]))
+        if keys[pygame.K_UP]:
+            if self.player.rotation == [0,-1]:
+                self.player.moveForward()
+            else: self.player.turnRight()
+        if keys[pygame.K_RIGHT]:
+            if self.player.rotation == [1,0]:
+                self.player.moveForward()
+            else: self.player.turnRight()
+        if keys[pygame.K_DOWN]:
+            if self.player.rotation == [0,1]:
+                self.player.moveForward()
+            else: self.player.turnRight()
+        if keys[pygame.K_LEFT]:
+            if self.player.rotation == [-1,0]:
+                self.player.moveForward()
+            else: self.player.turnRight()
+
+    def isInBounds(self, x, y):
+        if x<0 or y<0 or x>self.width-1 or y>self.height-1: return False
+        else: return True
+
+    def getObjectAt(self, x, y):
+        if self.isInBounds(x, y) == False: return False
+        else: return self.objectsCoordinates[x][y]
+       
+    def addObjectAt(self, x, y, obj):
+        if self.getObjectAt(x, y)!=None:
+            return
+        obj.cords = [x, y]
+        self.objectsCoordinates[x][y]=obj
+        self.objects.append(obj)
+
 
 world = World(SIM_WIDTH, SIM_HEIGHT)
-world.objectsCoordinates = [[None for _ in range(SIM_HEIGHT)] for _ in range(SIM_WIDTH)]
-
 
 class Cell:
-    def __init__(self, cords, lifeTime=100, genome=None):
+    def __init__(self, cords, maxLifeTime=100, genome=None):
         self.cords = cords
-        world.objectsCoordinates[int(cords[0])][int(cords[1])] = self
         self.rotation = [1, 0]
-        self.lifeTime = lifeTime
+        self.maxLifeTime = maxLifeTime
+        self.lifeTime = maxLifeTime
         self.energy = 50 # тратится на действие, если 0 - организм погибает
         self.isAlife = True
 
@@ -67,19 +106,12 @@ class Cell:
         #self.genome = self.generateGenome()
         self.genomeStep = 0 # убрать потом?
         
-        blue = 0
+        stepAmount = [0, 0, 0]
         for i in range(len(self.genome)):
-            if self.genome[i] < 20:
-                blue+=1
-        red = 0
-        for i in range(len(self.genome)):
-            if 80 <= self.genome[i] < 100:
-                red+=1
-        green = 0
-        for i in range(len(self.genome)):
-            if 65 <= self.genome[i] < 80:
-                green+=1
-        self.color = (255//len(self.genome)*red, 255//len(self.genome)*green, 255//len(self.genome)*blue)
+            if 80 <= self.genome[i] < 100: stepAmount[0]+=1
+            if 65 <= self.genome[i] < 80: stepAmount[1]+=1
+            if self.genome[i] < 20: stepAmount[2]+=1
+        self.color = (255//len(self.genome)*stepAmount[0], 255//len(self.genome)*stepAmount[1], 255//len(self.genome)*stepAmount[2])
 
 
         #self.speed = speed # или max speed и speed выбирается отдельно
@@ -92,6 +124,12 @@ class Cell:
         self.lifeTime-=1
         if self.energy<=0 or self.lifeTime==0: self.die()
 
+    def die(self):
+        self.isAlife = False
+        world.objectsCoordinates[int(self.cords[0])][int(self.cords[1])]=None
+
+        #print("cell died, energy:", self.energy, "lifetime:", self.lifeTime)
+
     def makeStep(self):
         s = self.genome[self.genomeStep]
         if 0 <= s <20: 
@@ -100,8 +138,10 @@ class Cell:
             self.turnLeft()
         if 25 <= s < 30: 
             self.turnRight()
-        if 30 <= s < 65 and len(world.objects) < SIM_WIDTH*SIM_HEIGHT:
+        if 30 <= s < 50 and len(world.objects) < SIM_WIDTH*SIM_HEIGHT:
             self.replicate()
+        if 50 <= s < 65:
+            self.visuallyDependentBehavior()
         if 65 <= s < 80:
             self.photosynthesize()
         if 80 <= s < 100:
@@ -113,22 +153,22 @@ class Cell:
     def moveForward(self):
         #сделать проверку на то заняты ли клетки между начальной и конечной (если добавить скорость)
 
-        old_x = self.cords[0]
-        old_y = self.cords[1]
+        old_x = int(self.cords[0])
+        old_y = int(self.cords[1])
 
-        target_x = self.cords[0]+self.rotation[0]
-        target_y = self.cords[1]+self.rotation[1]
+        target_x = int(self.cords[0]+self.rotation[0])
+        target_y = int(self.cords[1]+self.rotation[1])
 
-        if int(target_x)<0 or int(target_y)<0 or int(target_x)>SIM_WIDTH-1 or int(target_y)>SIM_HEIGHT-1: return
+        if world.isInBounds(target_x, target_y) == False: return
 
-        if world.objectsCoordinates[int(target_x)][int(target_y)]!=None: return
+        if world.getObjectAt(target_x, target_y)!=None or False: return
         
 
-        self.cords[0]=int(target_x)
-        self.cords[1]=int(target_y)
+        self.cords[0]=target_x
+        self.cords[1]=target_y
 
         world.objectsCoordinates[int(self.cords[0])][int(self.cords[1])]=self
-        world.objectsCoordinates[int(old_x)][int(old_y)]=None
+        world.objectsCoordinates[old_x][old_y]=None
 
     def turnRight(self):
         if self.rotation==[1,0]:self.rotation=[0,1]
@@ -143,15 +183,15 @@ class Cell:
         elif self.rotation==[0,1]: self.rotation=[1,0]
 
     def replicate(self):
-        if self.energy<30:
+        if self.energy<20:
             #print("not enough energy")
             return
-        target_x = self.cords[0]+self.rotation[0]
-        target_y = self.cords[1]+self.rotation[1]
-        if int(target_x)<0 or int(target_y)<0 or int(target_x)>SIM_WIDTH-1 or int(target_y)>SIM_HEIGHT-1:
-            return
-        if world.objectsCoordinates[int(target_x)][int(target_y)]!=None:
-            return
+        target_x = int(self.cords[0]+self.rotation[0])
+        target_y = int(self.cords[1]+self.rotation[1])
+
+        if world.isInBounds(target_x, target_y) == False: return
+
+        if world.getObjectAt(target_x, target_y)!=None or False: return
 
         #print("new cell added")
         newGenome = self.genome.copy()
@@ -161,25 +201,18 @@ class Cell:
             if newGenome[i]>100:newGenome[i] = newGenome[i]-100
             elif newGenome[i]<0:newGenome[i] = newGenome[i]+100
 
-        newLifeTime = self.lifeTime
-        if random.randint(0,99)>50:
-            newLifeTime+=random.randint(-10,10)
-        if newLifeTime<=0: newLifeTime=20
+        newMaxLifeTime = self.maxLifeTime
+        if random.randint(0,99)>90:
+            newMaxLifeTime+=random.randint(-10,10)
+        if newMaxLifeTime<=0: newMaxLifeTime=20
 
-        newCell = Cell([self.cords[0]+self.rotation[0], self.cords[1]+self.rotation[1]], genome=newGenome, lifeTime=newLifeTime)
+        newCell = Cell([self.cords[0]+self.rotation[0], self.cords[1]+self.rotation[1]], genome=newGenome, maxLifeTime=newMaxLifeTime)
         newCell.rotation = self.rotation
 
         newCell.energy = self.energy//2
         self.energy = self.energy//2
 
-        world.objects.append(newCell)
-
-    def die(self):
-        self.isAlife = False
-        world.objectsCoordinates[int(self.cords[0])][int(self.cords[1])]=None
-
-        #print("cell died, energy:", self.energy, "lifetime:", self.lifeTime)
-
+        world.addObjectAt(target_x, target_y, newCell)
 
     def generateRandomGenome(self, length): 
         return [random.randint(0,100) for _ in range(length)]
@@ -189,19 +222,36 @@ class Cell:
         if self.energy>50: self.energy=50
 
     def biteCell(self):
+        target_x = int(self.cords[0]+self.rotation[0])
+        target_y = int(self.cords[1]+self.rotation[1])
+
+        if world.isInBounds(target_x, target_y) == False: return
+
+        if world.getObjectAt(target_x, target_y)==None or False: return
+
+        target_cell = world.objectsCoordinates[int(target_x)][int(target_y)]
+        if target_cell.energy<1:
+            return
+
+        self.energy+=target_cell.energy//2 
+        target_cell.energy = target_cell.energy//2
+
+    def visuallyDependentBehavior(self):
         target_x = self.cords[0]+self.rotation[0]
         target_y = self.cords[1]+self.rotation[1]
-        if int(target_x)<0 or int(target_y)<0 or int(target_x)>SIM_WIDTH-1 or int(target_y)>SIM_HEIGHT-1:
-            return
+        if world.isInBounds(int(target_x), int(target_y)) == False: return
+
         if world.objectsCoordinates[int(target_x)][int(target_y)]==None:
             return
-        target_cell = world.objectsCoordinates[int(target_x)][int(target_y)]
-        if target_cell.energy<10:
-            return
+        else:
+            for i in range(len(self.genome)//4):
+                self.genomeStep+=1
+                if self.genomeStep > len(self.genome)-1: self.genomeStep=0
 
-        target_cell.energy-=10
-        self.energy+=10
-
+def saveData(obj, name):
+    path = f"{Path(__file__).parent.absolute()}/{name}.json"
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(obj, f, indent=4, ensure_ascii=False)
 
 pygame.init()
 screen = pygame.display.set_mode((SIM_WIDTH * multiplier, SIM_HEIGHT * multiplier))
@@ -209,8 +259,12 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-world.objects = [Cell(pygame.Vector2(random.randint(0, SIM_WIDTH-1), random.randint(0, SIM_HEIGHT-1))) for _ in range(200)]
-world.player = Cell([SIM_WIDTH/2, SIM_HEIGHT/2])
+timeToSave = 0
+dataToSave = []
+timesSaved = 0
+
+world.objects = [Cell([random.randint(0, SIM_WIDTH-1), random.randint(0, SIM_HEIGHT-1)]) for _ in range(200)]
+world.player = Cell([SIM_WIDTH//2, SIM_HEIGHT//2])
 #world.player.genome = [...] # для порождения клеток с заданным геномом
 
 while running:
@@ -228,7 +282,21 @@ while running:
 
     for i in range(len(world.objects)):
         pygame.draw.rect(screen, world.objects[i].color, (world.objects[i].cords[0] * multiplier, world.objects[i].cords[1] * multiplier, multiplier, multiplier))
-    pygame.draw.rect(screen, "blue", (world.player.cords[0] * multiplier, world.player.cords[1] * multiplier, multiplier, multiplier))
+    pygame.draw.rect(screen, "white", (world.player.cords[0] * multiplier, world.player.cords[1] * multiplier, multiplier, multiplier))
+
+    timeToSave+=1
+    cellsData=[]
+    if timeToSave>=100:
+        for i in world.objects:
+            cellsData.append([i.maxLifeTime, i.genome])
+        dataToSave.append([timesSaved, cellsData])
+        timesSaved+=1
+        timeToSave=0
+        print(timesSaved)
+
+    if timesSaved>=300: 
+        saveData(dataToSave, "result")
+        running=False
 
     dt = clock.tick(1000) / 1000.0 # limits FPS to n fps
     pygame.display.flip()
